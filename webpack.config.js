0b29ff8f450953af3e6906179c3498692c927933
itHy <1324876445@qq.com>
2022-06-17 19:43:06 +0800

    完成webpack-dev-server功能！

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
+    mode: 'production',
     entry: './src/main.js',
     output: {
         path: path.join(__dirname, 'lib'),
